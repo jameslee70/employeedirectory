d391281425420ff92bf2d6d3ccb5f501b7ec5b4c
James Lee <61759291+jameslee70@users.noreply.github.com>
2020-12-02 14:43:00 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from "./componets/wrapper/wrapper"
+import './app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      < Wrapper />
+    </div>
+  );
+}
+
+export default App;
